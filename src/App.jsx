8dd962d4ba40ea95e9b2f13bb9b50f8a16200e86
Alfandhi <chandrafajar356@gmail.com>
2024-05-07 10:49:45 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePages from './pages/HomePages';
 import ProductPages from './pages/ProductPages';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import ForgotPassword from './pages/auth/ForgotPassword';
 import DetailPages from './pages/DetailPages';
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+  const handleRouteChange = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  };
+
+  useEffect(() => {
+    handleRouteChange();
+  }, [location]);
   return (
     <>
+      {isLoading && <Loading />}
       <Routes>
         <Route path="/" element={<HomePages />} />
         <Route path="/product" element={<ProductPages />} />
